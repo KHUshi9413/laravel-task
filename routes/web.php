4da27b30704e1e3039bd0bfacaa98a6cef2b2163
Your Name <you@example.com>
2025-11-28 10:40:57 +0530

    Updated routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,40 +13,25 @@ use App\Http\Controllers\ProjectController;
 Route::get('/', function () {
     return redirect()->route('register.form');
 });
-
+//login routes 
 Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
-
-
+//register routes 
 Route::get('/register', [RegisterController::class, 'showRegister'])->name('register.form');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
-
-
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware('auth')
-    ->name('dashboard');
-
-
-
+//roles route 
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
-    
+Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 });
-
-
-Route::get('/manager', [DashboardController::class, 'manager'])
-    ->middleware(['auth', 'role:manager'])
-    ->name('manager');
-Route::get('/userpanel', [DashboardController::class, 'userPanel'])
-    ->middleware('auth')
-    ->name('user.panel');
-
-
+Route::get('/manager', [DashboardController::class, 'manager'])->middleware(['auth', 'role:manager'])->name('manager');
+Route::get('/userpanel', [DashboardController::class, 'userPanel'])->middleware('auth')->name('user.panel');
+// task route 
 Route::resource('/tasks', TaskController::class);
-     Route::resource('projects', ProjectController::class);
+//project route 
+Route::resource('projects', ProjectController::class);
+//document route 
 Route::get('/documents', [DocumentController::class, 'index'])->name('documents.index');
 Route::get('/documents/create', [DocumentController::class, 'create'])->name('documents.create');
 Route::post('/documents/store', [DocumentController::class, 'store'])->name('documents.store');
